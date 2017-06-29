58e1b283ee9958b9bfc06ab0ddcc22ecfdc15965
RobertoGalois <victor.vague@live.fr>
2017-06-29 19:03:48 +0200

    Update secure.js

--- secure.js
+++ secure.js
@@ -1,5 +1,8 @@
 function secure(pString)
 {	
-	var ret = (pString.replace(/[^a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]/, "_"));
+	var ret = (pString.replace(/[^a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]/g, "_"));
 	return ret;
 }
+
+//arrow
+const secure2 = pString => (pString.replace(/[^a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]/g, "_"));
